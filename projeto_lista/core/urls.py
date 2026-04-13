4c744d41069dda58a6599c21ae3c82a1ac2e14dd
0001181604-spec <000181604@senaimgaluno.com.br>
2026-04-13 18:44:57 -0300

    ok

--- projeto_lista/core/urls.py
+++ projeto_lista/core/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('login/', views.meu_login, name='login'),
     path('tarefas/', views.tarefa_list, name='tarefa-list'),  
     path('logout/', views.meu_logout, name='logout'), 
+    path('tarefas/nova/', views.tarefa_create, name='tarefa-create'),
+path('tarefas/<int:pk>/editar/', views.tarefa_update, name='tarefa-edit'),
+path('tarefas/<int:pk>/excluir/', views.tarefa_delete, name='tarefa-delete'),
 ]
--- projeto_lista/core/views.py
+++ projeto_lista/core/views.py
@@ -3,7 +3,8 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .models import Tarefa
-
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import TarefaForm
 def meu_login(request):
     if request.method == 'POST':
         username = request.POST.get('username')
@@ -26,3 +27,37 @@ def meu_logout(request):
 def tarefa_list(request):
     tarefas = Tarefa.objects.all()
     return render(request, 'core/tarefa_list.html', {'tarefas': tarefas})
+
+@login_required
+def tarefa_create(request):
+    if request.method == 'POST':
+        form = TarefaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Tarefa criada com sucesso!')
+            return redirect('tarefa-list')
+    else:
+        form = TarefaForm()
+    return render(request, 'core/tarefa_form.html', {'form': form})
+
+@login_required
+def tarefa_update(request, pk):
+    tarefa = get_object_or_404(Tarefa, pk=pk)
+    if request.method == 'POST':
+        form = TarefaForm(request.POST, instance=tarefa)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Tarefa atualizada com sucesso!')
+            return redirect('tarefa-list')
+    else:
+        form = TarefaForm(instance=tarefa)
+    return render(request, 'core/tarefa_form.html', {'form': form})
+
+@login_required
+def tarefa_delete(request, pk):
+    tarefa = get_object_or_404(Tarefa, pk=pk)
+    if request.method == 'POST':
+        tarefa.delete()
+        messages.success(request, 'Tarefa excluída com sucesso!')
+        return redirect('tarefa-list')
+    return render(request, 'core/tarefa_confirm_delete.html', {'tarefa': tarefa})       
