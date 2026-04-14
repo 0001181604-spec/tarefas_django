67bfb1cb81fc577d5eed1e1dab85f4a32be7c8dc
0001181604-spec <000181604@senaimgaluno.com.br>
2026-04-13 21:30:22 -0300

    ok

--- projeto_lista/projeto_lista/urls.py
+++ projeto_lista/projeto_lista/urls.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from core.api_views import TarefaViewSet, CategoriaViewSet, PrioridadeViewSet
+
+router = DefaultRouter()
+router.register(r'tarefas', TarefaViewSet)
+router.register(r'categorias', CategoriaViewSet)
+router.register(r'prioridades', PrioridadeViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls')),
     path('', include('core.urls')),
 ]
